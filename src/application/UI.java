e09c6c6555a62f2646f28225efdf4b9e1c239f80
Ryan Souza <ryanssebadelhe@gmail.com>
2025-11-03 14:35:46 -0300

    Placing pieces on the board

--- src/application/UI.java
+++ src/application/UI.java
@@ -8,7 +8,7 @@ public class UI {
 		
 		for (int i=0; i < pieces.length;i++ ) {
 			System.out.print((8 - i) + " ");
-			for (int j=0; j<pieces.length;j++) {
+			for (int j=0; j<pieces[i].length;j++) {
 				printPiece(pieces[i][j]);
 			}
 			System.out.println();
