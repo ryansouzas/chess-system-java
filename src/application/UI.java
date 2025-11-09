66de07d538ce2610b04e2c9e4a861b5738839365
Ryan Souza <ryanssebadelhe@gmail.com>
2025-11-09 01:32:40 -0300

    Check Logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -59,6 +59,9 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println("Turn: " + chessMatch.getTurn());
 		System.out.println("Waiting Player: "+ chessMatch.getCurrentPlayer());
+		if(chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
@@ -106,12 +109,12 @@ public class UI {
 		List<ChessPiece> black = captured.stream().filter(x -> x.getColor() == Color.BLACK).collect(Collectors.toList());
 		System.out.println("Captured pieces: ");
 		System.out.print("White: ");
-		System.out.println(ANSI_WHITE);
-		System.out.println(Arrays.toString(white.toArray()));
+		System.out.print(ANSI_WHITE);
+		System.out.print(Arrays.toString(white.toArray()));
 		System.out.println(ANSI_RESET);
 		System.out.print("Black: ");
-		System.out.println(ANSI_YELLOW);
-		System.out.println(Arrays.toString(black.toArray()));
+		System.out.print(ANSI_YELLOW);
+		System.out.print(Arrays.toString(black.toArray()));
 		System.out.println((ANSI_RESET));
 	}
 
